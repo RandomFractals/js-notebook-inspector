e63593f37cfccfb6e8e2f1d659a28b339667e257
Taras Novak <taras.novak@randomfractals.com>
2020-04-30 20:51:40 -0500

    #6 added file utils for local and remote data and text reads

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,5 @@ import { LogLevel } from "./logger";
 
 // log level setting for publishing vs. dev run of this ext.
 export const logLevel: LogLevel = LogLevel.Info; // change to .Debug for dev ...
+
+export const supportedDataFiles: RegExp = /.*\.(csv|json|geojson|topojson)/;
